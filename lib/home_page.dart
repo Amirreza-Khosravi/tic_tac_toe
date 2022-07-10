baa406aa73fbc2c95d730504a0f9c1f9effb4f5d
Amirreza <amirrezakhosravi.dev@gmail.com>
2022-07-10 12:49:50 +0430

    scoreBoard ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,11 +8,78 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Tic Tac Toe'),
+        title: Text(
+          'Tic Tac Toe',
+          style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         backgroundColor: Colors.grey[900],
         elevation: 0,
       ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Column(
+              children: [
+                getScoreBord(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getScoreBord() {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player X',
+                  style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+              Text(
+                '0',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player O',
+                  style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+              Text(
+                '0',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
